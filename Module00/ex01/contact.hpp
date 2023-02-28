86567bb3b1077b961c67050cbedf5c2ea92ea7e3
Noppadon Piya-isarakul <npiya-is@z3t8c1.42bangkok.com>
2023-02-28 19:57:01 +0700

    add phonebook class

---
+++ Module00/ex01/contact.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npiya-is <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 17:25:27 by npiya-is          #+#    #+#             */
+/*   Updated: 2023/02/28 17:33:36 by npiya-is         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_CLASS_H
+# define CONTACT_CLASS_H
+
+#include <iostream>
+#include <string>
+
+class Contact {
+	std :: string	firstname;
+	std :: string	lastname;
+	std :: string	nickname;
+	std :: string	darkest_secret;
+	int		number;
+
+}
+
+#endif
---
+++ Module00/ex01/phonebook.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npiya-is <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 16:08:35 by npiya-is          #+#    #+#             */
+/*   Updated: 2023/02/28 17:49:55 by npiya-is         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_CLASS_H
+# define PHONEBOOK_CLASS_H
+
+#include "contact.hpp"
+
+class PhoneBook : public Contact {
+	Contact contact[8];
+	public:
+		ADD(sting input)
+}
+
+#endif
