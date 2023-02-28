5367c97bbde285ef3fb0e013562c320a52f7a103
Noppadon Piya-isarakul <npiya-is@z3t8c1.42bangkok.com>
2023-02-28 20:25:11 +0700

    add method

--- Module00/ex01/phonebook.cpp
+++ Module00/ex01/phonebook.cpp
@@ -6,12 +6,14 @@
 /*   By: npiya-is <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 16:08:26 by npiya-is          #+#    #+#             */
-/*   Updated: 2023/02/28 20:18:51 by npiya-is         ###   ########.fr       */
+/*   Updated: 2023/02/28 20:23:55 by npiya-is         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
 void PhoneBook::ADD(std::string input) {
-	
+	for(Contact con: contact) {
+		std:: cout << con.firstname << std::endl;
+	}
 }
