8a3b3f73807aae6146e24d6d8aca4c7a4c1cc8df
Noppadon Piya-isarakul <npiya-is@z3t8c4.42bangkok.com>
2023-04-07 15:09:56 +0700

    add more example in Module02 ex02

--- Module02/ex02/main.cpp
+++ Module02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: npiya-is <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 22:52:29 by npiya-is          #+#    #+#             */
-/*   Updated: 2023/04/02 16:54:34 by npiya-is         ###   ########.fr       */
+/*   Updated: 2023/04/07 15:05:45 by npiya-is         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,22 @@
 int main( void ) {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
+	Fixed const c( Fixed( 5.05f ) / Fixed( 2 ) );
+	Fixed const d( Fixed( 5.05f ) + Fixed( 2 ) );
+	Fixed const e( Fixed( 5.05f ) - Fixed( 2 ) );
+	std::cout << "a : " << a << std::endl;
+	std::cout << "b : " << b << std::endl;
+	std::cout << "c : " << c << std::endl;
+	std::cout << "d : " << d << std::endl;
+	std::cout << "e : " << e << std::endl;
+	std::cout << "++a equal "<< ++a << std::endl;
+	std::cout << "a++ equal " << a++ << std::endl;
+	std::cout << "after a++ a equal " << a << std::endl;
 	if (a >= b)
 		std::cout << "a >= b" << std::endl;
 	else
 		std::cout << "a < b" << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
-	std::cout << Fixed::min( a, b ) << std::endl;
+	std::cout << "Max value between a and b is " << Fixed::max( a, b ) << std::endl;
+	std::cout << "Min value between a and b is " << Fixed::min( a, b ) << std::endl;
 	return 0;
 }
